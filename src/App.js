69807ba48f7f6435a69abb48e6ab17188d34e820
NoahKoh <kohyusheng@gmail.com>
2024-09-20 01:49:44 +0800

    Add MovieSearcher Project
    - Set up React project structure
    - Implemented movie search functionality with the use of OMDB API.
    - Create Moviecard component to display movie details
    - Add search input and button with event handlers
    - Default search is 'Batman' on initial load

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import Moviecard from './Moviecard';
+import SearchIcon from './search.svg';
+import './App.css';
+
+const API_URL = 'http://www.omdbapi.com/?apikey=418ed69b';
+
+
+function App() {
+
+  const [movies, setMovie] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovie(data.Search);
+  }
+  
+  useEffect(() =>{
+    searchMovie('Batman');
+  }, []);
+  
+  return (
+    <div className="app">
+      <h1>MovieSeacher</h1>
+      <div className="search">
+        <input type="text" placeholder="Search movie..." 
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          onKeyDown={(e) => {if (e.key === 'Enter') searchMovie(searchValue)}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovie(searchValue)}/>
+      </div>
+
+      {movies?.length > 0
+        ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <Moviecard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
+      <h2>Created by NoahKoh</h2>
+    </div>
+  );
+}
+
+export default App;
